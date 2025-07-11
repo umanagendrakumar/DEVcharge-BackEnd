384bb1c855829b67fa0691a65702e2a56585d449
umanagendrakumar <your.email@example.com>
2025-07-11 14:18:14 +0530

    Replaced throw error with empty response

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
         const { token } = req.cookies;
 
         if (!token) {
-            throw new Error("Please login to access");
+            return res.status(401).send("Please login to access");
         }
 
         const decodedMessage = await jwt.verify(token, "privateKey");
