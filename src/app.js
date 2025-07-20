b31e3082e5d942df439ff7244ec8272ba681b9e6
umanagendrakumar <your.email@example.com>
2025-07-20 13:17:21 +0530

    Console removed & Chat Feature Added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const http = require("http");
+const { Socket } = require("socket.io");
+
 const { connectDB } = require("./database.js");
 
 
@@ -13,13 +16,11 @@ const allowedOrigins = ["http://localhost:5173", "https://devcharge.netlify.app"
 
 app.use(cors({
     origin: function (origin, callback) {
-        console.log("Origin received by CORS:", origin); // 👈 Add this
         if (!origin) return callback(null, true);
 
         if (allowedOrigins.includes(origin)) {
             return callback(null, true);
         } else {
-            console.log("❌ CORS blocked:", origin); // 👈 Add this
             return callback(new Error("Not allowed by CORS"));
         }
     },
@@ -34,16 +35,24 @@ const { authRouter } = require("./routes/auth.js");
 const { profileRouter } = require("./routes/profile.js");
 const { requestRouter } = require("./routes/request.js");
 const { userRouter } = require("./routes/user.js");
+const chatRouter = require("./routes/chat.js");
+
+const initializeSocket = require("./utils/socket.js");
+
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server, allowedOrigins);
 
 
 connectDB()
     .then(() => {
         console.log("Database connected successfully");
-        app.listen(process.env.PORT, () => {
+        server.listen(process.env.PORT, () => {
             console.log("Server is listening at port 7777...");
         })
     })
